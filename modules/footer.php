234655060f88fe3ce33f02b1df03e23c82e42266
Brian <bdlowery2@gmail.com>
2021-12-04 23:41:12 -0800

    add style-guide to footer.

--- modules/footer.php
+++ modules/footer.php
@@ -2,9 +2,14 @@
   <h3 class="regular-voice"><strong>Brian Lowery</strong></h3>
   <p>Last updated <time datetime="2021-11-25"> November 25th, 2021</time></p>
 </div>
-<nav class="site-menu footer-links">
-  <a href="https://www.linkedin.com/in/bdlowery/">LinkedIn</a>
-  <a href="https://twitter.com/bdlowery2">Twitter</a>
-  <a href="https://codepen.io/bdlowery">CodePen</a>
-  <a href="https://github.com/bdlowery">GitHub</a>
-</nav>
+<div class="footer-links-group">
+  <nav class="site-menu footer-links">
+    <a href="?page=style-guide" class="">style-guide</a>
+  </nav>
+  <nav class="site-menu footer-links">
+    <a href="https://www.linkedin.com/in/bdlowery/">LinkedIn</a>
+    <a href="https://twitter.com/bdlowery2">Twitter</a>
+    <a href="https://codepen.io/bdlowery">CodePen</a>
+    <a href="https://github.com/bdlowery">GitHub</a>
+  </nav>
+</div>
