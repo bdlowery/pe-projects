92e336a63c4df383473b1aaf4a638a30ad805fdc
Brian <bdlowery2@gmail.com>
2021-12-04 19:43:42 -0800

    remove old page-header module. Include new module

--- experiments.php
+++ experiments.php
@@ -1,9 +1,2 @@
 <?php include("project-database.php") ?>
-<header class="page-header">
-  <div class="inner-column">
-    <h1 class="title-voice">
-      Experiments
-    </h1>
-    <p>Welcome to my experimental lab... Watch out, you may find some super cool stuff!</p>
-  </div>
-</header>
+<?php include("./modules/page-header.php") ?>
