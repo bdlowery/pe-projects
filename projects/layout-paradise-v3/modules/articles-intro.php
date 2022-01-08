790e3b7b4ef4008d1f7ba307b2872efc724691f0
Brian <bdlowery2@gmail.com>
2022-01-07 18:07:02 -0700

    add id to article-card intro for hash link

--- projects/layout-paradise-v3/modules/articles-intro.php
+++ projects/layout-paradise-v3/modules/articles-intro.php
@@ -2,11 +2,11 @@
 
   <header>
     <?php if ($theme === "couple") { ?>
-      <h2 class='attention-voice'>Tailored Experiences</h2>
+      <h2 class='attention-voice' id="card-intro">Tailored Experiences</h2>
 
       <p class='calm-voice'>Designed to ensure you feel inspired and can unwind in harmony, no matter if you come as a couple, family, or group, and no matter the age or desires.</p>
     <?php } else { ?>
-      <h2 class='attention-voice'>Endless Entertainment</h2>
+      <h2 class='attention-voice' id="card-intro">Endless Entertainment</h2>
 
       <p class='calm-voice'>Children laughing and smiling as they splash in the waves. The bliss of a long-awaited, well-deserved spa day. We offer something special for everyone.</p>
     <?php } ?>
