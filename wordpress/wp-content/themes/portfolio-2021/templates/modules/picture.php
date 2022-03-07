92126f4791957ef4758023ae8301ac04aae219be
Brian <bdlowery2@gmail.com>
2022-03-07 15:36:02 -0700

    Add conditional logic for intro paras with pics

--- wordpress/wp-content/themes/portfolio-2021/templates/modules/picture.php
+++ wordpress/wp-content/themes/portfolio-2021/templates/modules/picture.php
@@ -2,11 +2,19 @@
 $imageHeading = get_sub_field("heading");
 $image = get_sub_field("image")["url"];
 $imageDescription = get_sub_field("description");
+$has_intro = get_sub_field("has_intro");
+$intro = get_sub_field("intro");
 ?>
 
 <section class="picture-module">
   <div class="inner-column">
-    <h2><?= $imageHeading; ?></h2>
+    <?php if ($imageHeading) { ?>
+      <h2><?= $imageHeading; ?></h2>
+    <?php } ?>
+
+    <?php if ($has_intro) { ?>
+      <?= $intro ?>
+    <?php } ?>
     <picture>
       <img src="<?= $image; ?>" alt="">
     </picture>
