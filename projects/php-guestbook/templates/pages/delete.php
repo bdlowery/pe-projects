c58bb9c9da10eb1cc0b0f7f4e91fc3997c6a0605
Brian <bdlowery2@gmail.com>
2022-03-23 02:28:49 -0600

    Add redirect after deleting element to index.php

--- projects/php-guestbook/templates/pages/delete.php
+++ projects/php-guestbook/templates/pages/delete.php
@@ -1,14 +1,15 @@
-<?php 
-  //retrieve value after &id=____
-  $id = $_GET["id"];
+<?php
+//retrieve value after &id=____
+$id = $_GET["id"];
 
-  //open the database
-  //check to see if id in url matches id in database
-  $comments = getComments()["items"];
-  foreach($comments as $commentId => $comment) {
-    if ($id == $commentId) {
-      deleteComment($id);
-    } else {
-      $error = "this comment does not exist";
-    }
+//open the database
+//check to see if id in url matches id in database
+$comments = getComments()["items"];
+foreach ($comments as $commentId => $comment) {
+  if ($id == $commentId) {
+    deleteComment($id);
+    header('Location: index.php');
+  } else {
+    header('Location: index.php');
   }
+}
