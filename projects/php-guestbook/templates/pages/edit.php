22221c9509306ebbbe39b2a0e030d4582327db25
Brian <bdlowery2@gmail.com>
2022-03-24 17:15:30 -0600

    Add inner-column to edit form

--- projects/php-guestbook/templates/pages/edit.php
+++ projects/php-guestbook/templates/pages/edit.php
@@ -4,16 +4,17 @@ $id = $_GET["id"];
 
 //open the database and retrieve the comments
 $comments = getComments()["items"];
-
-//check if the id in the url matches the id in the json
-foreach ($comments as $commentId => $comment) {
-  if ($id == $commentId) {
-    include("templates/modules/edit-form.php");
-  }
-}
-
 ?>
-
 <section>
-  edit.php
+  <div class="inner-column">
+    <?php
+    //check if the id in the url matches the id in the json
+    foreach ($comments as $commentId => $comment) {
+      if ($id == $commentId) {
+        include("templates/modules/edit-form.php");
+      }
+    }
+
+    ?>
+  </div>
 </section>
