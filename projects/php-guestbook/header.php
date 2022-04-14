ac9189fe8a13f572c93f3fdcedef3df24011423f
Brian <bdlowery2@gmail.com>
2022-04-14 00:07:29 -0600

    Add perpetual education link in navigation

--- projects/php-guestbook/header.php
+++ projects/php-guestbook/header.php
@@ -5,6 +5,8 @@
         <h1 class="loud-voice">P<span class="small">(E)</span>HP Guestbook</h1>
         <nav class="site-menu">
           <a href="?page=home" class="site-link">Home</a>
+          <a href="?page=about" class="site-link">About</a>
+          <a href="https://perpetual.education/" target="_blank" rel="nofollow noopener noreferrer">Perpetual Education</a>
           <!-- <a href="?page=comments">All comments</a>
         <a href="?page=add-comment">Add comment</a> -->
         </nav>
