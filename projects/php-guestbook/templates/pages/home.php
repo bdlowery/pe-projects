7104e2f065131453a26f75a4fd80799cede2b830
Brian <bdlowery2@gmail.com>
2022-04-14 00:10:30 -0600

    Put display-comments module on homepage only.

--- projects/php-guestbook/templates/pages/home.php
+++ projects/php-guestbook/templates/pages/home.php
@@ -3,3 +3,10 @@
     <?php include("./templates/modules/add-comment.php") ?>
   </div>
 </section>
+
+<section class="display-comments">
+  <div class="inner-column">
+    <?php include("./templates/modules/display-comments.php") ?>
+
+  </div>
+</section>
