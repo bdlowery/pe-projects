81ffbe7fec4e721b35fb6916273467475253989b
Brian <bdlowery2@gmail.com>
2022-04-15 01:13:20 -0600

    change css in head to site-min.css

--- projects/php-guestbook/head.php
+++ projects/php-guestbook/head.php
@@ -10,6 +10,10 @@
   <meta name="description" content="P(E)HP Guestbook" />
   <meta property="og:image" content="[[meta image source]]" />
 
+  <!-- script for changing theme of site. load first to prevent flash -->
+  <!-- <script src="./toggleTheme.js"></script> -->
+
+
   <!-- Styles -->
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="site-min.css" />
 </head>
