1266e475629ed23b04b2af1d3c0271428bee15ab
Brian <bdlowery2@gmail.com>
2022-04-21 23:26:50 -0600

    Add array of colors to randomly generate avatar

--- projects/php-guestbook/head.php
+++ projects/php-guestbook/head.php
@@ -22,4 +22,49 @@
     //   body.dataset.theme = window.localStorage.getItem("theme");
     // }
   </script>
+
+  <?php
+  $colors = [
+    "#D8BFD8",
+    "#A0522D",
+    "#483D8B",
+    "#66CDAA",
+    "#DEB887",
+    "#FFE4E1",
+    "#2E8B57",
+    "#7B68EE",
+    "#90EE90",
+    "#228B22",
+    "#FF7F50",
+    "#EE82EE",
+    "#A52A2A",
+    "#4B0082",
+    "#000000",
+    "#AFEEEE",
+    "#D3D3D3",
+    "#9370DB",
+    "#7FFFD4",
+    "#7CFC00",
+    "#40E0D0",
+    "#FF6347",
+    "#FFE4B5",
+    "#20B2AA",
+    "#FFF0F5",
+    "#FF8C00",
+    "#800000",
+    "#98FB98",
+    "#F08080",
+    "#FFDAB9"
+  ]
+  ?>
+
+  <style type="text/css">
+    <?php
+    $i = 1;
+    foreach ($colors as $color) {
+      echo ".comment-group:nth-of-type(" . count($colors) . "n+" . $i . ")::before { background-color: " . $color . "}";
+      $i++;
+    }
+    ?>
+  </style>
 </head>
