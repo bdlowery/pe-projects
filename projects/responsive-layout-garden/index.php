8ee9d51fb66dd75eb7025e70e89588b50b144232
Brian <bdlowery2@gmail.com>
2022-08-14 01:03:41 -0600

    Scaffold new structure

--- projects/responsive-layout-garden/index.php
+++ projects/responsive-layout-garden/index.php
@@ -6,10 +6,31 @@ showErrors();
 
 include('header.php');
 
+$json = file_get_contents('home.json');
+$page = json_decode($json, true);
+$count = 0;
 
-include('templates/modules/module-1.php');
-include('templates/modules/module-2.php');
-include('templates/modules/module-3.php');
+foreach($page['sections'] as $section) { 
+  $id = $section['type'] . '-' . $count;
+  $name = $section['name'];
+?>
+  <section class="<?=$name?>" id="<?=$id?>">
+    <div class="inner-column">
+
+      <?php include("templates/modules/$section[type].php"); ?>
+
+    </div>
+  </section>
+<?php $count++ ?>
+
+<?php } ?>
+
+
+
+<?php 
+
+// include('templates/modules/module-2.php');
+// include('templates/modules/module-3.php');
 
 
 
---
+++ projects/responsive-layout-garden/templates/modules/graphic-diptych.php
@@ -0,0 +1,17 @@
+<?php 
+	$heading = $section['heading'] ?? 'Something that will instantly grab your attention';
+ ?>
+
+
+
+<graphic-diptych>
+	<div class="text-content">
+		<h2 class='title-voice'><?=$heading?></h2>
+		<p class='regular-voice'>A good company with a cool product, we can make it happen when it matters most so you can count on us, I promise.</p>
+		<a href="#" class='button'>Find Out More</a>
+	</div>
+	
+	<picture>
+		<img src="images/mirror.jpg" alt="">
+	</picture>
+</graphic-diptych>
